e320847bb07de0433463609743592c689578d66d
João Marcos <joaomarcosm.silva@outlook.com>
2022-11-14 01:04:20 -0300

    change base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://challenge-openbanking.herokuapp.com",
 });
-
-/* https://challenge-openbanking.herokuapp.com */
